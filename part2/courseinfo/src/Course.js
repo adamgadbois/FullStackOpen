58ad8e25932167cd8379d74dabfeecf2b33a020d
Adam Gadbois <adamgadbois@outlook.com>
2022-04-20 10:49:17 -0500

    completed exercise

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,19 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => parts.map(part=><Part part={part}/>)
+
+const Course = ({course}) =>
+  <>
+    <Header course={course.name}/>
+    <Content parts={course.parts}/>
+    <Total sum={course.parts.reduce((s,p)=>s+p.exercises,0)}/>
+  </>
+
+export default Course
